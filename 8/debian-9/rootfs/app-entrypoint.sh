9c61e45c4f25e4a7923231151e01f731c0a3fffa
Luca Gattoni <luca.gattoni@deptagency.com>
2019-02-19 01:37:57 +0000

    fixed entrypoint.sh

--- 8/debian-9/rootfs/app-entrypoint.sh
+++ 8/debian-9/rootfs/app-entrypoint.sh
@@ -17,14 +17,14 @@ if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "httpd" ]]; then
 	
 	echo Drupal has been initialized.;
   
-	if [ -d /bitnami/drupal/vendor ]; then
-		rm -rf /opt/bitnami/drupal/vendor
-	else
-		mv /opt/bitnami/drupal/vendor /bitnami/drupal/vendor
-	fi
-  
-	ln -s /bitnami/drupal/vendor /opt/bitnami/drupal/vendor
-	echo Created vendor symlink.;
+#	if [ -d /bitnami/drupal/vendor ]; then
+#		rm -rf /opt/bitnami/drupal/vendor
+#	else
+#		mv /opt/bitnami/drupal/vendor /bitnami/drupal/vendor
+#	fi
+#  
+#	ln -s /bitnami/drupal/vendor /opt/bitnami/drupal/vendor
+#	echo Created vendor symlink.;
 
 	ln -s /bitnami/drupal/libraries /opt/bitnami/drupal/libraries
 	echo Created libraries symlink.;
