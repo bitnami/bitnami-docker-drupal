d22b556e383c4192e27b4127f00a8b38dc10fd6d
Luca Gattoni <luca.gattoni@deptagency.com>
2019-02-19 00:10:45 +0000

    added persistent "vendor" folder

--- 8/debian-9/rootfs/app-entrypoint.sh
+++ 8/debian-9/rootfs/app-entrypoint.sh
@@ -6,9 +6,27 @@
 print_welcome_page
 
 if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "httpd" ]]; then
-  . /init.sh
-  nami_initialize apache php mysql-client libphp drupal
-  info "Starting drupal... "
+
+	. /init.sh
+	nami_initialize apache php mysql-client libphp drupal
+  
+	until [ -f /bitnami/drupal/.initialized ]; do
+    	echo Drupal is still initializing...
+    	sleep 5
+	done
+	
+	echo Drupal has been initialized.;
+  
+	if [ -d /bitnami/drupal/vendor ]; then
+		rm -rf /opt/bitnami/drupal/vendor
+	else
+		mv /opt/bitnami/drupal/vendor /bitnami/drupal/vendor
+	fi
+  
+	ln -s /bitnami/drupal/vendor /opt/bitnami/drupal/vendor
+	echo Created vendor symlink.;
+  
+	info "Starting drupal... "
 fi
 
 exec tini -- "$@"
